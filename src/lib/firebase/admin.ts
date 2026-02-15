fde95d001b5156c3b453bbb5a7417ee820a32879
tushig tse <tsetushig@gmail.com>
2026-02-15 06:18:03 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,15 +1,13 @@
 import 'server-only';
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-);
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+  : undefined;
 
 export const adminApp =
   getApps().find((app) => app.name === 'admin') ||
   initializeApp(
-    {
-      credential: cert(serviceAccount),
-    },
+    serviceAccount ? { credential: cert(serviceAccount) } : undefined,
     'admin'
   );
