9591a21381b0fa9b46fc26ef56f313b35d4af7f1
tushig tse <tsetushig@gmail.com>
2026-02-16 10:15:19 +0000

    logo nii link iig soliy : https://files.catbox.moe/dg3rej.png

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn('relative block h-10 w-[240px]', className)}>
-      <Image src="https://i.postimg.cc/mDDB9tqC/Bullsara-1.png" alt="Bullsara Logo" fill priority sizes="240px" />
+      <Image src="https://files.catbox.moe/dg3rej.png" alt="Bullsara Logo" fill priority sizes="240px" />
     </Link>
   );
 }
